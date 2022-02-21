d6818250cb075c600c69c3ae7f4780cef47b5c68
kapza <66725494+BitterAlmondJuice@users.noreply.github.com>
2022-02-21 20:36:13 +0100

    la madre que parió a las listas enlazadas
    me he quedado en lo de usar el metodo mostrar() de la clase Asignaturas en el metodo mostrar() de la clase pila.
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    :(
    uwu

---
+++ ED 2 Practica. PilasColas/src/Asignatura.java
@@ -0,0 +1,39 @@
+public class Asignatura {
+
+    private String nombre;
+
+    private double calificacion;
+
+    public Asignatura(String nombre, double calificacion) {
+
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+    }
+
+    public String getNombre() {
+
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+    }
+
+    public double getCalificacion() {
+
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+
+        this.calificacion = calificacion;
+    }
+
+    public void mostrar() {
+
+        System.out.println(getNombre() + " (" + getCalificacion() + ")");
+    }
+
+
+}
---
+++ ED 2 Practica. PilasColas/src/Principal.java
@@ -0,0 +1,60 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Practica ED Pilas y Colas de: " + "Fernando García Pérez");
+        System.out.println("Numero Matricula: " + "BS0076");
+        Principal pruebas = new Principal();
+
+        Pila asignaturas = pruebas.prepararPila();
+        pruebas.pruebasPila(asignaturas);
+
+        Cola grupo = pruebas.prepararCola();
+        pruebas.pruebasCola(grupo);
+    }
+
+    public Pila prepararPila() {
+        Pila asignaturas = new Pila();
+        Asignatura ffi = new Asignatura("Fundamentos Físicos de la Informática", 3.50);
+        Asignatura ed = new Asignatura("Estructuras de Datos", 6.35);
+        Asignatura aes = new Asignatura("Aspectos Éticos y Sociales", 7.70);
+        Asignatura fs = new Asignatura("Fundamentos de seguridad", 6.75);
+        asignaturas.apilar(ffi);
+        asignaturas.apilar(ed);
+        asignaturas.apilar(aes);
+        asignaturas.apilar(fs);
+        return asignaturas;
+    }
+
+    public void pruebasPila(Pila asignaturas) {
+        asignaturas.mostrar(); //esto no funciona bien, estará seguramente relacionado con el metodo "mostrar" de la clase "pila"
+    }
+
+    public Cola prepararCola() {
+        Cola grupo = new Cola();
+        /*Alumno a1 = new Alumno("Felipe Arias Gonzalez", "aa1253");
+        Alumno a2 = new Alumno("Manuel Garcia Sacedón", "ax0074");
+        Alumno a3 = new Alumno("Margarita Lopez Medina", "mj7726");
+        Alumno a4 = new Alumno("Estela Sanchez Arellano", "bc2658");
+        Asignatura ffi = new Asignatura("Fundamentos Físicos de la Informática", 3.50);
+        Asignatura ed = new Asignatura("Estructuras de Datos", 6.35);
+        Asignatura aes = new Asignatura("Aspectos Éticos y Sociales", 7.70);
+        Asignatura fs = new Asignatura("Fundamentos de seguridad", 6.75);
+        a1.anadirAsignatura(ed);
+        ed = new Asignatura("Estructuras de Datos", 4.35);
+        a2.anadirAsignatura(ed);
+        ed = new Asignatura("Estructuras de Datos", 7.5);
+        a3.anadirAsignatura(ed);
+        ed = new Asignatura("Estructuras de Datos", 2.5);
+        Asignatura algebra = new Asignatura("Álgebra", 5);
+        a4.anadirAsignatura(algebra);
+        a4.anadirAsignatura(fs);
+        grupo.encolar(a1);
+        grupo.encolar(a2);
+        grupo.encolar(a3);
+        grupo.encolar(a4);*/
+        return grupo;
+    }
+
+    public void pruebasCola(Cola grupo) {
+        //Completar
+	}
+}
